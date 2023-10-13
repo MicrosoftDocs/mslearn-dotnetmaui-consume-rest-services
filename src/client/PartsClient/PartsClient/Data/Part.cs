b56395a99febeadd73cbc69355cd52707b3d2120
James Montemagno <james.montemagno@gmail.com>
2023-10-13 16:49:58 -0700

    more cleanup

--- src/client/PartsClient/PartsClient/Data/Part.cs
+++ src/client/PartsClient/PartsClient/Data/Part.cs
@@ -1,38 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace PartsClient.Data;
 
-namespace PartsClient.Data
+[Serializable]
+public class Part
 {
-    [Serializable]
-    public class Part
+    public string PartID { get; set; }
+
+    public string PartName { get; set; }
+
+    public string TheSuppliers { get; set; }
+
+    public string PartType { get; set; }
+
+    public List<string> Suppliers { get; set; } = new List<string>();
+    public DateTime PartAvailableDate { get; set; }
+
+    public string SupplierString
     {
-        public string PartID { get; set; }
-
-        public string PartName { get; set; }
-
-        public string TheSuppliers { get; set; }
-
-        public string PartType { get; set; }
-
-        public List<string> Suppliers { get; set; } = new List<string>();
-        public DateTime PartAvailableDate { get; set; }
-
-        public string SupplierString
+        get
         {
-            get
+            string result = String.Empty;
+            foreach (string supplier in Suppliers)
             {
-                string result = String.Empty;
-                foreach (string supplier in Suppliers)
-                {
-                    result += $"{supplier}, ";
-                }
-                result = result.Trim(',', ' ');
-                return result;
+                result += $"{supplier}, ";
             }
+            result = result.Trim(',', ' ');
+            return result;
         }
     }
 }
